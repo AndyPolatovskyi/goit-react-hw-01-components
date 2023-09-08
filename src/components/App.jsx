224487babe55f71dcec52c7488f57b5ba0f4db41
AndyPolatovskyi <d25121978b@gmail.com>
2023-09-08 04:51:25 +0300

    08

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,22 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import css from 'App.module.css';
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
+        
+        // fontSize: 40,
+        // color: '#010101',
       }}
     >
-      <div>
+      <div className={css.container}>
         <Profile
           username={user.username}
           tag={user.tag}
@@ -29,9 +31,9 @@ export const App = () => {
         />
         <Statistics title="Upload stats" stats={data} />
         
-        <FriendList friends={friends} />;
-        <TransactionHistory items={transactions} />;
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
       </div>
-    </div>
+      </div>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -28,5 +28,12 @@ export const TransactionHistory = ({ items }) => {
 }
 
 TransactionHistory.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.string)
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  )
 }
